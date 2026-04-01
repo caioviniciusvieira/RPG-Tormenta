1e590acc3cf79182ce0370991534c8b51fc1f87e
caioviniciusvieira <caioviniciusrsvieira15@gmail.com>
2026-04-01 16:56:44 +0000

    Pericias de exemplo, botão rolar dados rola um 1d20 e soma os atributos,t reinos, 1/2nível e outros

---
+++ Tormenta/scripts/talentos.js
@@ -0,0 +1,23 @@
+
+
+
+
+function rolar_dado(botao){
+    const linha = botao.closest("tr")
+    const input_atributo = linha.querySelector('.atributo').value 
+    const input_nivel = linha.querySelector('.nivel').value 
+    const input_treino = linha.querySelector('.treino').value 
+    const input_outros = linha.querySelector('.outros').value 
+
+    console.log(input_nivel,input_atributo,input_outros,input_treino)
+    var soma = (parseInt(input_atributo)+parseInt(input_nivel)+parseInt(input_outros)+parseInt(input_treino))
+    function dado() {
+        return Math.floor(Math.random() * 20) + 1;
+    }
+    valor = dado()
+    resultado = valor + soma
+    console.log("Rolagem do dado",valor)
+    console.log("Valor",resultado)
+
+    window.alert(`1d20+[${soma}] = ${resultado}`)
+}
